3ba4d90def442446928431f0d1a304182ba3850c
SheepmaanDev <sheepmaandev@gmail.com>
2024-11-19 16:44:07 +0100

    Create

---
+++ scsi/src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.sass'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>�� 2022 Your Company. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ scsi/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/header/header'
+
+import './home.sass'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Header />
+        </div>
+    )
+}
+
+export default Home
