06ed3b57f9eed63242cc07d4d5639e4308ee59f6
SheepmaanDev <sheepmaandev@gmail.com>
2024-11-21 17:25:18 +0100

    add section

---
+++ scsi/src/components/service/service.jsx
@@ -0,0 +1,14 @@
+// import 
+
+import './service.sass'
+
+const Service = () => {
+    return (
+        <section className='service'>
+            <h2 className='service__title'>Nos Services</h2>
+
+        </section>
+    )
+}
+
+export default Service
---
+++ scsi/src/data/services.js
@@ -0,0 +1,78 @@
+import Repair from '../assets/repair.png'
+import Accompaniement from '../assets/accompaniement.png'
+import Training from '../assets/training.png'
+import Sale from '../assets/sale.png'
+import Wifi from '../assets/wifi.png'
+
+export const ServicesList = [
+    {
+        id: '01',
+        img: Repair,
+        title: 'Réparration',
+        desc: 'Que vous rencontriez des problèmes matériels ou logiciels, notre service de réparation est conçu pour répondre à toutes vos exigences.'
+    },
+    {
+        id: '02',
+        img: Accompaniement,
+        title: 'Accompagnement',
+        desc: 'Que ce soit pour résoudre des pannes matérielles complexes, optimiser les performances de votre système ou simplement vous guider dans l\'utilisation de vos équipements, notre équipe est là pour vous accompagner à chaque étape.'
+    },
+    {
+        id: '03',
+        img: Training,
+        title: 'Formation',
+        desc: 'Que vous soyez un particulier souhaitant améliorer ses compétences informatiques ou une entreprise cherchant à former ses employés, notre service de formation personnalisée est adapté à vos besoins spécifiques.'
+    },
+    {
+        id: '04',
+        img: Sale,
+        title: 'Vente',
+        desc: 'Que vous ayez besoin de remplacer un équipement défectueux ou d\'investir dans de nouveaux outils pour améliorer votre productivité, notre équipe est là pour vous conseiller et vous fournir les solutions les plus adaptées à vos besoins et à votre budget.'
+    },
+    {
+        id: '05',
+        img: Wifi,
+        title: 'Réseaux et wifi',
+        desc: 'Que vous souhaitiez améliorer la couverture Wi-Fi dans votre maison, votre entreprise ou tout autre espace, notre équipe d\'experts est là pour évaluer vos besoins, concevoir une solution sur mesure et procéder à une installation professionnelle pour garantir des performances optimales.'
+    },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+    // {
+    //     id: '',
+    //     title: '',
+    //     desc: ''
+    // },
+]
